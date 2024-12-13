521cd16e32a57a7ad14c5e194f5c1f7d660f9049
sroooooor60 <sroooooor90@gmail.com>
2024-12-13 20:01:34 +0200

    Add Note Buttom Sheet

--- lib/views/notes_view.dart
+++ lib/views/notes_view.dart
@@ -1,14 +1,25 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:nots_app/widgets/add_note_buttom_sheet.dart';
 import 'package:nots_app/widgets/notes_view_body.dart';
 
 class NotesView extends StatelessWidget {
   const NotesView({super.key});
 
+  final List<PopupMenuEntry> fofo = const [];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
+        onPressed: () {
+          showModalBottomSheet(
+            context: context,
+            builder: (context) {
+              return AddNoteButtomSheet();
+            },
+          );
+        },
         child: Icon(Icons.add),
       ),
       body: const NotesViewBody(),
---
+++ lib/widgets/add_note_buttom_sheet.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AddNoteButtomSheet extends StatelessWidget {
+  const AddNoteButtomSheet({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(),
+    );
+  }
+}
