858b8ce10b23f15c438d2144fbda75815b53b4e2
sroooooor60 <sroooooor90@gmail.com>
2024-12-24 20:44:29 +0200

    add button to buttom sheet

--- lib/widgets/add_note_buttom_sheet.dart
+++ lib/widgets/add_note_buttom_sheet.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:nots_app/constants.dart';
+import 'package:nots_app/widgets/custom_button.dart';
 import 'package:nots_app/widgets/custom_text_feild.dart';
 
 class AddNoteButtomSheet extends StatelessWidget {
@@ -8,22 +10,28 @@ class AddNoteButtomSheet extends StatelessWidget {
   Widget build(BuildContext context) {
     return const Padding(
       padding: EdgeInsets.symmetric(horizontal: 16),
-      child: Column(
-        children: [
-          SizedBox(
-            height: 32,
-          ),
-          CustomTextField(
-            hintText: 'title',
-          ),
-          SizedBox(
-            height: 16,
-          ),
-          CustomTextField(
-            hintText: 'content',
-            maxLines: 6,
-          ),
-        ],
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 32,
+            ),
+            CustomTextField(
+              hintText: 'title',
+            ),
+            SizedBox(
+              height: 16,
+            ),
+            CustomTextField(
+              hintText: 'content',
+              maxLines: 5,
+            ),
+            SizedBox(
+              height: 32,
+            ),
+            CustomButton(),
+          ],
+        ),
       ),
     );
   }
---
+++ lib/widgets/custom_button.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:nots_app/constants.dart';
+
+class CustomButton extends StatelessWidget {
+  const CustomButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      height: 55,
+      decoration: BoxDecoration(
+        color: KprimaryColor,
+        borderRadius: BorderRadius.circular(8),
+      ),
+      child: const Center(
+          child: Text(
+        'Add',
+        style: TextStyle(
+            color: Colors.black, fontSize: 24, fontWeight: FontWeight.bold),
+      )),
+    );
+  }
+}
