f31bc81ee0e63253e69a3ed31bded9feb6f7273f
sroooooor60 <sroooooor90@gmail.com>
2024-12-28 17:59:07 +0200

    creat addNotes method to AddNotCubit

--- lib/cubits/add_notes_cubit/add_notes_cubit.dart
+++ lib/cubits/add_notes_cubit/add_notes_cubit.dart
@@ -1,11 +1,23 @@
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:nots_app/constants.dart';
 import 'package:nots_app/cubits/add_notes_cubit/add_notes_stats.dart';
 import 'package:nots_app/models/not_model.dart';
 
 class AddNotesCubit extends Cubit<AddNotesStats> {
   AddNotesCubit() : super(AddNotesInitial());
 
-  addNote(NotModel note) {}
+  addNote(NotModel note) async {
+    emit(AddNotesLoading());
+
+    try {
+      var notesBox = Hive.box<NotModel>(kNotsBox);
+      await notesBox.add(note);
+      emit(AddNotesSucces());
+    } catch (e) {
+      AddNotesFaliar(errorMessag: e.toString());
+    }
+  }
 }
 
 
