c43253da56dff30c22fc473525fd3c6d07ef75bd
sroooooor60 <sroooooor90@gmail.com>
2024-12-30 21:41:48 +0200

    add isScrollControlled to showModalButtomSheet & add kebord padding

--- lib/widgets/add_note_buttom_sheet.dart
+++ lib/widgets/add_note_buttom_sheet.dart
@@ -26,10 +26,14 @@ class AddNoteButtomSheet extends StatelessWidget {
           }
         },
         builder: (context, state) {
+          print('kebord = ${MediaQuery.of(context).viewInsets.bottom}');
           return AbsorbPointer(
             absorbing: state is AddNotesLoading ? true : false,
-            child: const Padding(
-              padding: EdgeInsets.symmetric(horizontal: 16),
+            child: Padding(
+              padding: EdgeInsets.only(
+                  left: 16,
+                  right: 16,
+                  bottom: MediaQuery.of(context).viewInsets.bottom),
               child: SingleChildScrollView(
                 child: AddNotForm(),
               ),
