1e33b08daee5178cdf676e10cc3829f91720cd7f
sroooooor60 <sroooooor90@gmail.com>
2025-01-03 20:55:18 +0200

    creat colors ListView

--- lib/widgets/color_list_view.dart
+++ lib/widgets/color_list_view.dart
@@ -1,19 +1,47 @@
 import 'package:flutter/material.dart';
 
 class ColorItem extends StatelessWidget {
-  const ColorItem({super.key});
+  const ColorItem({super.key, required this.isActive, required this.color});
+
+  final bool isActive;
+
+  final Color color;
 
   @override
   Widget build(BuildContext context) {
-    return const CircleAvatar(
-      backgroundColor: Colors.blue,
-      radius: 32,
-    );
+    return isActive
+        ? CircleAvatar(
+            backgroundColor: Colors.white,
+            radius: 32,
+            child: CircleAvatar(
+              backgroundColor: color,
+              radius: 30,
+            ),
+          )
+        : CircleAvatar(
+            backgroundColor: color,
+            radius: 32,
+          );
   }
 }
 
-class ColorsListView extends StatelessWidget {
+class ColorsListView extends StatefulWidget {
   const ColorsListView({super.key});
+
+  @override
+  State<ColorsListView> createState() => _ColorsListViewState();
+}
+
+class _ColorsListViewState extends State<ColorsListView> {
+  int currentIndex = 0;
+
+  List<Color> colors = const [
+    Color(0xff563635),
+    Color(0xff5B6057),
+    Color(0xff6E9075),
+    Color(0xff7B9A82),
+    Color(0xff87A38D),
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -21,9 +49,21 @@ class ColorsListView extends StatelessWidget {
       height: 32 * 2,
       child: ListView.builder(
         scrollDirection: Axis.horizontal,
-        itemCount: 10,
+        itemCount: colors.length,
         itemBuilder: (context, index) {
-          return ColorItem();
+          return Padding(
+            padding: EdgeInsets.symmetric(horizontal: 4),
+            child: GestureDetector(
+              onTap: () {
+                currentIndex = index;
+                setState(() {});
+              },
+              child: ColorItem(
+                color: colors[index],
+                isActive: currentIndex == index,
+              ),
+            ),
+          );
         },
       ),
     );
